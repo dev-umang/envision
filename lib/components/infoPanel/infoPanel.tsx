6fcb47ccb371cce7af0a758c8c2fb28b362e42c1
Umang Joshi <dev.umangjoshi@gmail.com>
2025-01-13 19:47:01 +0530

    Icon added

--- lib/components/infoPanel/infoPanel.tsx
+++ lib/components/infoPanel/infoPanel.tsx
@@ -1,15 +1,19 @@
 import { FC } from "react";
 import s from "./infoPanel.module.scss";
 import { InfoPanelProps } from "./info.props";
+import UGIcon from "../icon/icon";
 
 const InfoPanel: FC<InfoPanelProps> = ({ show, setShow, data }) => {
   return (
     <div className={s.panel} style={{ opacity: show ? 1 : 0 }}>
       <div className={s.header}>
         <strong>Mode Info</strong>
-        <button onClick={() => setShow?.(false)} className={s.hideBtn}>
-          Hide
-        </button>
+        <div>
+          <UGIcon icon="slow" />
+          <button onClick={() => setShow?.(false)} className={s.hideBtn}>
+            Hide
+          </button>
+        </div>
       </div>
       {!data && <div className={s.body}>Info not available!</div>}
       <div className={s.body}>
